432a57a8f4902f670ee5ff486ef691305cebc55b
Han434 <112228783+Han434@users.noreply.github.com>
2024-08-31 18:13:14 +0630

    Add expiration

--- src/pages/inventory/InventoryList.js
+++ src/pages/inventory/InventoryList.js
@@ -2,10 +2,46 @@ import React from 'react'
 import inventoryProducts from '../../Data/inventory.json';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import NewReleasesIcon from '@mui/icons-material/NewReleases';
-import { Button, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import { Button, IconButton, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 
 const LowStockAlert = ({ quantity, lowStock }) => {
-    return quantity <= lowStock ? <VerifiedIcon color='success'/> : <NewReleasesIcon color='error'/>;
+    return quantity > lowStock ? <ThumbUpAltIcon color='success'/> : <ReportProblemIcon color='error'/>;
+}
+
+function isDateInRange(date, startDate, endDate) {
+    return date >= startDate && date <= endDate;
+  }
+
+function willExpireInXMonths(expirationDate, months) {
+    let expiration = new Date(expirationDate);
+    const today = new Date();
+    const timeLater = new Date(today.getFullYear(), today.getMonth() + months, today.getDate());
+    // console.log("Time Later: " + timeLater);
+    // console.log("Today: " + today);
+    // console.log("Expiration: " + expiration);
+    return isDateInRange(expiration, today, timeLater) ? true: false;
+}
+
+function hasExpiredInXMonths(expirationDate, months) {
+    let expiration = new Date(expirationDate);
+    const today = new Date();
+    const timeLater = new Date(today.getFullYear(), today.getMonth() - months, today.getDate());
+    console.log("Time Later: " + timeLater);
+    console.log("Today: " + today);
+    console.log("Expiration: " + expiration);
+    return isDateInRange(expiration, timeLater, today) ? true: false;
+}
+
+const ExpirationDateAlert = ({ expirationDate }) => {    
+    if (willExpireInXMonths(expirationDate, 3)) {
+        return (<NewReleasesIcon color='warning'/>);
+    } else if (hasExpiredInXMonths(expirationDate, 11)) {
+        return (<NewReleasesIcon color='error'/>);
+    } else {
+        return (<VerifiedIcon color='success'/>);
+    }
 }
 
 const InventoryList = () => {
@@ -41,7 +77,6 @@ const InventoryList = () => {
                 <Table sx={{minWidth: "100%"}}>
                     <TableHead>
                         <TableRow>
-                            <TableCell align="center">Created Date</TableCell>
                             <TableCell align="center">Supplier</TableCell>
                             <TableCell align="center">Brand</TableCell>
                             <TableCell align="center">Description</TableCell>
@@ -50,13 +85,14 @@ const InventoryList = () => {
                             <TableCell align="center">Code</TableCell>
                             <TableCell align="center">Quantity</TableCell>
                             <TableCell align="center">LowStock</TableCell>
-                            <TableCell align="center">Status</TableCell>
+                            <TableCell align="center">Stock Status</TableCell>
+                            <TableCell align="center">Expiration Date</TableCell>
+                            <TableCell align="center">Expiration Status</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {inventoryProducts.map((inventory, index) => (
                             <TableRow key={index}>
-                                <TableCell align="center">{inventory.product.createdAt}</TableCell>
                                 <TableCell align="center">{inventory.product.supplier}</TableCell>
                                 <TableCell align="center">{inventory.product.brand}</TableCell>
                                 <TableCell align="center">{inventory.product.description}</TableCell>
@@ -67,6 +103,10 @@ const InventoryList = () => {
                                 <TableCell align="center">{inventory.lowStock}</TableCell>
                                 <TableCell align="center">
                                     <LowStockAlert quantity={inventory.quantity} lowStock={inventory.lowStock} />
+                                </TableCell>
+                                <TableCell align="center">{inventory.expirationDate}</TableCell>
+                                <TableCell align="center">
+                                    <ExpirationDateAlert expirationDate={inventory.expirationDate} />
                                 </TableCell>
                             </TableRow>
                         ))}
